57c50a235485468c99f2262525e3f091e05e47b2
muindor <contact@muindor.com>
2020-03-20 23:10:26 +0300

    remove ui; add api base

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function (Request $request) {
+    if ($request->user() === null) {
+        return response()->json(['error' => 'You are guest']);
+    }
+
+    return response()->json($request->user());
 });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', static function (Request $request) {
+    return response(['error' => "Unknown route '{$request->path()}'"]);
+})->where('any', '.*');
 
