c47055aee5cbbb00180dbc52990bd0052aac1ea2
Egor Muindor Fadeev <contact@muindor.com>
2020-04-11 14:09:12 +0300

    Удалены отладочные пути

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/debug/get_group', function () {
-    //$group = Group::whereTitle('171-372')->first();
-    //UpdateGroupRaspJob::dispatch($group, $session = false);
-});
-Route::get('/debug/farm', function () {
-    \App\Jobs\UpdateGroupListJob::dispatch()->chain([new \App\Jobs\UpdateScheduleJob()]);
-});
-Route::get('/debug/get_groups', function () {
-    // \App\Jobs\UpdateGroupListJob::dispatch();
-    return response()->json(Group::all());
-});
-Route::get('/debug/get_group/{group_title}', function ($group_title) {
-    $group = Group::whereTitle($group_title)->with(['lessons' => function ($query) {
-        /** @var $query Illuminate\Database\Eloquent\Relations\HasMany */
-        $query->orderBy('day_number')->orderBy('lesson_number')->orderBy('date_from');
-    }])->get();
-
-    return response()->json($group);
-});
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
